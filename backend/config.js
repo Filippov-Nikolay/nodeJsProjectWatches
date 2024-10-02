53bb9dba3583e6013f812cb3341b20adfb5cc55e
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-10-02 13:06:41 +0300

    add check authentication

---
+++ backend/config.js
@@ -0,0 +1,23 @@
+var mssql = require('mssql');
+
+// Параметры соединения с бд
+var config = {
+    user: 'admin',                      // пользователь базы данных
+    password: 'admin',                  // пароль пользователя 
+    server: 'LAPTOP-31VSBGAE',          // хост
+    database: 'Watches',               // имя бд
+    port: 1433,                         // порт, на котором запущен sql server
+    options: {
+        encrypt: true,                  // Использование SSL/TLS
+        trustServerCertificate: true    // Отключение проверки самоподписанного сертификата
+    },
+}
+
+// Connection
+var connection = new mssql.ConnectionPool(config); 
+
+var pool = connection.connect(function(err) {
+	if (err) console.log(err)
+}); 
+
+module.exports = pool; 
--- backend/server.js
+++ backend/server.js
@@ -8,19 +8,80 @@ var bodyParser = require('body-parser');
 
 var port = 8080;
 
+// connection
+var connection = require('./config');
+
 // Указываем Express, что файлы из папки public должны быть доступны как статические
 // Для Css
 app.use(express.static(path.join(__dirname, '../public')));
+
+// URL
+app.use(express.urlencoded({ extended: true }));
 
 // ejs
 app.set('views', path.join(__dirname, '../pages'));
 app.set('view engine', 'ejs');
 
-// connection
-// var connection = require('./js/config');
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+
+app.get('/signIn', function(req, res) {
+    res.sendFile(path.join(__dirname, "../signIn.html"));
+});
+
+app.post('/authentication', function(req, res) {
+    const login = req.body.login;
+    const password = req.body.password;
+
+    console.log(`${login}, ${password}`);
+
+    var ps = new mssql.PreparedStatement(connection);
+    ps.input('login', mssql.NVarChar);
+    ps.input('password', mssql.NVarChar);
+
+    let query = `
+        SELECT login, 'admin' AS userType
+        FROM Admins
+        WHERE Admins.login = @login AND Admins.password = @password
+
+        UNION
+
+        SELECT login, 'user' AS userType
+        FROM Users 
+        WHERE Users.login = @login AND Users.password = @password
+    `;
+
+    let insert = {
+        login: login,
+        password: password
+    }
+
+    ps.prepare(query, (err) => {
+        if (err) { console.error('Error preparing statement:', err); }
+
+        ps.execute(insert, (err, data) => {
+            if (err) { console.error('Error executing prepared statement:', err); }
+
+            console.log(data.recordset[0]);
+
+            if (data.recordset.length != 0) {
+                const user = data.recordset[0];
+
+                if (user.userType == 'admin') {
+                    res.send('admin');
+                } else if (user.userType == 'user') {
+                    res.send('user');
+                }
+            } else {
+                res.status(401).json({ error: 'Invalid login or password' });
+            }
+            ps.unprepare();
+        });
+    });
 });
 
 
